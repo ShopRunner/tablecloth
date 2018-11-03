e0b68f87bb57eb334661913c8803aa9ae6c9b627
SethRosenbauer <seth.rosenbauer@kenwayconsulting.com>
2018-11-03 17:48:59 -0500

    fix linting errors

--- src/lib/__tests__/Model.test.js
+++ src/lib/__tests__/Model.test.js
@@ -6,7 +6,7 @@ const Schema = require('../Schema');
 describe('Model.constructor()', () => {
   it('properly sets the options to properties', () => {
     const exampleSchema = new Schema();
-    const exampleTableName = "exampleTable";
+    const exampleTableName = 'exampleTable';
     const exampleModel = new Model(exampleTableName, exampleSchema);
     expect(exampleModel.tableName).toEqual(exampleTableName);
     expect(exampleModel.schema).toBe(exampleSchema);
--- src/lib/__tests__/TableCloth.test.js
+++ src/lib/__tests__/TableCloth.test.js
@@ -1,13 +1,12 @@
 'use strict';
 
 const Schema = require('../Schema');
-const Model = require('../Model');
 const TableCloth = require('../TableCloth');
 
 describe('TableCloth.model()', () => {
   it('returns a instantiated Model', () => {
     const exampleSchema = new Schema();
-    const exampleTableName = "exampleTable";
+    const exampleTableName = 'exampleTable';
     const returnValue = TableCloth.model(exampleTableName, exampleSchema);
     expect(returnValue.schema).toBe(exampleSchema);
     expect(returnValue.tableName).toEqual(exampleTableName);
